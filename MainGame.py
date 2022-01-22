8698776b8348c1029b553cc8d7fb81459e66131b
ThibaudARoy <thibone14@gmail.com>
2022-01-22 16:58:14 -0500

    rudy

---
+++ MainGame.py
@@ -0,0 +1,21 @@
+import pygame
+
+pygame.init() #init
+
+#create the screen
+
+screen = pygame.display.set_mode((800,600))
+
+#Title and Icon
+
+pygame.display.set_caption("DayLite")
+
+icon = pygame.image.load('zombie1.png')
+pygame.display.set_icon(icon)
+
+#Game Loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running= False
